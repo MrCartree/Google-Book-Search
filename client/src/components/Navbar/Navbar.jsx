9509c2962887e024409b940e340c1e63f5c6d965
Zach LaFleur <zach.lafleur95@gmail.com>
2020-12-24 13:07:30 -0700

    added a few style changes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="Navbar">
       <div className="Navbar-brand">
-        <span>GOOGLE BOOK SEARCH</span>
+        <span>GOOGLE BOOKS SEARCH</span>
       </div>
 
       <ul className="Navbar-links">
